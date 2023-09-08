16fe276b35c1ce7c6a0347dc9c7c473ccb32f1ac
tzigor <igortz@mail.ru>
2023-09-08 16:54:28 +0700

    End of file

--- main.pas
+++ main.pas
@@ -58,7 +58,7 @@ type
     Value: String8;
   end;
 
-  TCongifData = record
+  TConfigData = record
     Name: String32;
     DataType: String2;
   end;
@@ -70,8 +70,10 @@ type
     hasDateTime: Boolean;
     hasVersion: Boolean;
     Version: Byte;
-    Data: array of TCongifData;
+    Data: array of TConfigData;
   end;
+
+  TDataConfiguration = array of TConfig;
 
 var
   App: TApp;
@@ -81,6 +83,8 @@ var
   currentFileSize: LongWord;
   EndOfFile: Boolean;
   Offset: LongWord;
+  DataConfiguration: TDataConfiguration;
+  RecordOffset: Word;
 
 implementation
 
@@ -162,19 +166,115 @@ begin
   Result:= CurrentRecord;
 end;
 
-function GetDateTimeStr(CurrentRecord: TCurrentRecord): TDateTime;
+function ReadDateTime(CurrentRecord: TCurrentRecord): TDateTime;
 begin
   with CurrentRecord do begin
     if (Not InArray(Cmd, NoDateTimeCmd)) And (N > 0) then
        Result:= EncodeDateTime(StrToInt(IntToHex(Data[0])), StrToInt(IntToHex(Data[1])), StrToInt(IntToHex(Data[2])),
                       StrToInt(IntToHex(Data[3])), StrToInt(IntToHex(Data[4])), StrToInt(IntToHex(Data[5])), 0);
   end;
+  Inc(RecordOffset, 6);
+end;
+
+function ReadVersion(CurrentRecord: TCurrentRecord): Byte;
+begin
+  Result:= CurrentRecord.Data[RecordOffset];
+  Inc(RecordOffset);
+end;
+
+function ReadByte(CurrentRecord: TCurrentRecord): Byte;
+begin
+  Result:= CurrentRecord.Data[RecordOffset];
+  Inc(RecordOffset);
+end;
+
+function CommandExist(Cmd: Byte): Boolean;
+var ConfigLen, i: Word;
+begin
+  ConfigLen:= Length(DataConfiguration);
+  for i:=0 to ConfigLen - 1 do
+     if Cmd = DataConfiguration[i].Cmd then begin
+        Result:= True;
+        Exit;
+     end;
+  Result:= False;
+end;
+
+function RecordWithDateTme(Cmd: Byte): Boolean;
+var ConfigLen, i: Word;
+begin
+  ConfigLen:= Length(DataConfiguration);
+  for i:=0 to ConfigLen - 1 do
+     if (Cmd = DataConfiguration[i].Cmd) And (DataConfiguration[i].hasDateTime) then begin
+        Result:= True;
+        Exit;
+     end;
+  Result:= False;
+end;
+
+function RecordWithVersion(Cmd: Byte): Boolean;
+var ConfigLen, i: Word;
+begin
+  ConfigLen:= Length(DataConfiguration);
+  for i:=0 to ConfigLen - 1 do
+     if (Cmd = DataConfiguration[i].Cmd) And (DataConfiguration[i].hasVersion) then begin
+        Result:= True;
+        Exit;
+     end;
+  Result:= False;
+end;
+
+function FindConfiguration(Cmd, Ver: Byte; WVersion: Boolean): Word;
+var ConfigLen, i: Word;
+begin
+  ConfigLen:= Length(DataConfiguration);
+  for i:=0 to ConfigLen - 1 do
+       if (Cmd = DataConfiguration[i].Cmd) And ((Ver = DataConfiguration[i].Version) Or Not WVersion) then begin
+          Result:= i;
+          Exit;
+       end;
+  Result:= 65535;
+end;
+
+function ReadParameter(ConfigData: TConfigData; CurrentRecord: TCurrentRecord): String;
+var wStr: String;
+begin
+  with CurrentRecord do begin
+     case ConfigData.DataType of
+       'i1', 'u1': wStr:= IntToStr(ReadByte(CurrentRecord));
+       'i2': begin
+               wStr:= IntToStr(FillInteger(Data[RecordOffset + 1], Data[RecordOffset]));
+               Inc(RecordOffset, 2);
+             end;
+       'u2': begin
+               wStr:= IntToStr(FillWord(Data[RecordOffset + 1], Data[RecordOffset]));
+               Inc(RecordOffset, 2);
+             end;
+       'i4': begin
+               wStr:= IntToStr(FillLongInt(Data[RecordOffset + 3], Data[RecordOffset + 2], Data[RecordOffset + 1], Data[RecordOffset]));
+               Inc(RecordOffset, 4);
+             end;
+       'u4': begin
+               wStr:= IntToStr(FillLongWord(Data[RecordOffset + 3], Data[RecordOffset + 2], Data[RecordOffset + 1], Data[RecordOffset]));
+               Inc(RecordOffset, 4);
+             end;
+       'f4': begin
+               wStr:= FloatToStrF(FillSingle(Data[RecordOffset + 3], Data[RecordOffset + 2], Data[RecordOffset + 1], Data[RecordOffset]), ffFixed, 10, 3);
+               Inc(RecordOffset, 4);
+             end;
+     end;
+     Result:= ConfigData.Name + '=' + wStr;
+  end;
 end;
 
 procedure PraseBin();
 var b: Byte;
     CurrentRecord: TCurrentRecord;
-    wStr, TimeStr: String;
+    wStr, pStr, TimeStr: String;
+    Version: Byte;
+    ConfigIndex, ConfigDataLen, i: Word;
+    RecDateTime: TDateTime;
+    WithVersion: Boolean;
 begin
   TimeStr:= '';
   if LoadBinFile then begin
@@ -183,8 +283,24 @@ begin
        while (b <> $C0) And (Not EndOfFile) do b:= GetCurrentByte;
        if b = $C0 then begin
           CurrentRecord:= GetCurrentRecord;
-          DateTimeToString(TimeStr, 'd-mmm-yy hh:nn:ss', GetDateTimeStr(CurrentRecord));
-          wStr:= wStr + TimeStr + '     Cmd: ' + IntToStr(CurrentRecord.Cmd) + '    N: ' + IntToStr(CurrentRecord.N) + NewLine;
+          RecordOffset:= 0;
+          if CommandExist(CurrentRecord.Cmd) then begin
+             if RecordWithDateTme(CurrentRecord.Cmd) then RecDateTime:= ReadDateTime(CurrentRecord);
+             WithVersion:= RecordWithVersion(CurrentRecord.Cmd);
+             if WithVersion then Version:= ReadVersion(CurrentRecord);
+             ConfigIndex:= FindConfiguration(CurrentRecord.Cmd, Version, WithVersion);
+             if ConfigIndex < 65536 then begin
+                ConfigDataLen:= Length(DataConfiguration[ConfigIndex].Data);
+                for i:=0 to ConfigDataLen - 1 do begin
+                   //pStr:= pStr + ReadParameter(DataConfiguration[ConfigIndex].Data[i], CurrentRecord) + '; ';
+                   ReadParameter(DataConfiguration[ConfigIndex].Data[i], CurrentRecord)
+                end;
+             end;
+
+             DateTimeToString(TimeStr, 'd-mmm-yy hh:nn:ss', RecDateTime);
+             //wStr:= wStr + TimeStr + '     Cmd: ' + IntToStr(CurrentRecord.Cmd) + '    Ver: ' + IntToStr(Version) + ' Index: ' + IntToStr(ConfigDataLen) + NewLine;
+             wStr:= wStr + TimeStr + '   ' + pStr + NewLine;
+          end;
        end;
      until EndOfFile;
      App.Memo.Text:= wStr;
@@ -199,31 +315,78 @@ begin
   ParamLen:= Length(Param);
   for i:= 1 to ParamLen do begin
     if Param[i] = ConfigParamSeparator then begin
-      GetConfigParam.Param:= Trim(wStr);
+      GetConfigParam.Param:= LowerCase(Trim(wStr));
       wStr:= '';
     end
     else wStr:= wStr + Param[i];
   end;
-  GetConfigParam.Value:= Trim(wStr);
+  GetConfigParam.Value:= LowerCase(Trim(wStr));
 end;
 
-function ParseConfig(): String;
-var i, j, LineLen: Word;
+procedure ParseConfig();
+var i, j, LineLen, ConfigCounter: Word;
     wStr, tStr: String;
+    Config: TConfig;
     ConfigParam: TConfigParam;
+    ConfigData: TConfigData;
 begin
+  SetLength(DataConfiguration, 0);
   for i:=0 to ConfigList.Count - 1 do begin
+     SetLength(Config.Data, 0);
      LineLen:= Length(ConfigList[i]);
+     ConfigCounter:= 0;
      for j:=1 to LineLen do begin
-        if ConfigList[i][j] = ConfigSeparator then begin
+        if (ConfigList[i][j] = ConfigSeparator) Or (j = LineLen) then begin
            ConfigParam:= GetConfigParam(wStr);
-           tStr:= tStr + ConfigParam.Param + ' ' + ConfigParam.Value + NewLine;
+           Inc(ConfigCounter);
+           case ConfigParam.Param of
+             'addr': Config.Addr:= StrToInt(ConfigParam.Value);
+             'cmd': Config.Cmd:= StrToInt(ConfigParam.Value);
+             'len': Config.N:= StrToInt(ConfigParam.Value);
+             'datetime': Config.hasDateTime:= StrToBool(ConfigParam.Value);
+             'ver': begin
+                      Config.hasVersion:= True;
+                      Config.Version:= StrToInt(ConfigParam.Value);
+                    end;
+           else
+             if ConfigParam.Param <> '' then begin
+                ConfigData.Name:= ConfigParam.Param;
+                ConfigData.DataType:= ConfigParam.Value;
+                Insert(ConfigData, Config.Data, ConfigCounter);
+             end;
+           end;
            wStr:= '';
         end
         else wStr:= wStr + ConfigList[i][j];
      end;
+     Insert(Config, DataConfiguration, i + 1);
   end;
-  Result:= tStr;
+end;
+
+function ConfigToStr(const DataConfiguration: TDataConfiguration): String;
+var ConfigLen, DataLen, i, j: Word;
+    wStr: String;
+begin
+  ConfigLen:= Length(DataConfiguration);
+  for i:=0 to ConfigLen - 1 do begin
+     wStr:= wStr + IntToStr(DataConfiguration[i].Addr) + NewLine;
+     wStr:= wStr + IntToStr(DataConfiguration[i].Cmd) + NewLine;
+     wStr:= wStr + IntToStr(DataConfiguration[i].N) + NewLine;
+     wStr:= wStr + IntToStr(DataConfiguration[i].Version) + NewLine;
+     DataLen:= Length(DataConfiguration[i].Data);
+     for j:=0 to DataLen - 1 do begin
+        wStr:= wStr + DataConfiguration[i].Data[j].Name + ': ' + DataConfiguration[i].Data[j].DataType + NewLine;
+     end;
+  end;
+  Result:= wStr;
+end;
+
+procedure LoadConfiguration();
+begin
+  if ConfigList is TStringList then FreeAndNil(ConfigList);
+  ConfigList:= TStringList.Create;
+  ConfigList.LoadFromFile('Config.dat');
+  ParseConfig;
 end;
 
 procedure TApp.LoagConfigClick(Sender: TObject);
@@ -234,13 +397,15 @@ begin
      if ConfigList is TStringList then FreeAndNil(ConfigList);
      ConfigList:= TStringList.Create;
      ConfigList.LoadFromFile(OpenDialog.FileName);
-     Memo.Text:= ParseConfig;
+     ParseConfig;
+     Memo.Text:= ConfigToStr(DataConfiguration);
   end;
 end;
 
 procedure TApp.OpenBinClick(Sender: TObject);
 begin
   App.Memo.Text:= '';
+  LoadConfiguration;
   PraseBin;
 end;
 
